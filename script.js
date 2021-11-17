7c90f62699a299d7c18d4b04f3540552dc8d8f8f
Ammarat <acuster91@mail.ru>
2021-11-17 16:24:17 +0600

    do lesson 4

---
+++ script.js
@@ -0,0 +1,21 @@
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+
+let state = 0;
+
+function render() {
+    counter.textContent = state.toString();
+}
+
+addBtn.addEventListener('click', () => {
+    state++;
+    render();
+})
+
+subBtn.addEventListener('click', () => {
+    state--;
+    render();
+})
+
+render();
